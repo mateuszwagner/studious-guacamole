ac2f99c1c8f110025aface702af3b63043c40ab6
saas <dupa>
2018-06-12 14:02:32 +0200

    aa

--- install/include/marker_publisher/Marker.h
+++ install/include/marker_publisher/Marker.h
@@ -45,6 +45,7 @@ struct Marker_
 
 
 
+
   typedef boost::shared_ptr< ::marker_publisher::Marker_<ContainerAllocator> > Ptr;
   typedef boost::shared_ptr< ::marker_publisher::Marker_<ContainerAllocator> const> ConstPtr;
 
@@ -76,7 +77,7 @@ namespace message_traits
 
 
 // BOOLTRAITS {'IsFixedSize': True, 'IsMessage': True, 'HasHeader': False}
-// {'geometry_msgs': ['/opt/ros/kinetic/share/geometry_msgs/cmake/../msg'], 'marker_publisher': ['/home/tatulo/Documents/studious-guacamole/src/marker_publisher-master/msg'], 'std_msgs': ['/opt/ros/kinetic/share/std_msgs/cmake/../msg']}
+// {'geometry_msgs': ['/opt/ros/kinetic/share/geometry_msgs/cmake/../msg'], 'marker_publisher': ['/home/saas/studious-guacamole/src/marker_publisher-master/msg'], 'std_msgs': ['/opt/ros/kinetic/share/std_msgs/cmake/../msg']}
 
 // !!!!!!!!!!! ['__class__', '__delattr__', '__dict__', '__doc__', '__eq__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_parsed_fields', 'constants', 'fields', 'full_name', 'has_header', 'header_present', 'names', 'package', 'parsed_fields', 'short_name', 'text', 'types']
 
--- install/include/marker_publisher/MarkerArray.h
+++ install/include/marker_publisher/MarkerArray.h
@@ -46,6 +46,7 @@ struct MarkerArray_
 
 
 
+
   typedef boost::shared_ptr< ::marker_publisher::MarkerArray_<ContainerAllocator> > Ptr;
   typedef boost::shared_ptr< ::marker_publisher::MarkerArray_<ContainerAllocator> const> ConstPtr;
 
@@ -77,7 +78,7 @@ namespace message_traits
 
 
 // BOOLTRAITS {'IsFixedSize': False, 'IsMessage': True, 'HasHeader': True}
-// {'geometry_msgs': ['/opt/ros/kinetic/share/geometry_msgs/cmake/../msg'], 'marker_publisher': ['/home/tatulo/Documents/studious-guacamole/src/marker_publisher-master/msg'], 'std_msgs': ['/opt/ros/kinetic/share/std_msgs/cmake/../msg']}
+// {'geometry_msgs': ['/opt/ros/kinetic/share/geometry_msgs/cmake/../msg'], 'marker_publisher': ['/home/saas/studious-guacamole/src/marker_publisher-master/msg'], 'std_msgs': ['/opt/ros/kinetic/share/std_msgs/cmake/../msg']}
 
 // !!!!!!!!!!! ['__class__', '__delattr__', '__dict__', '__doc__', '__eq__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_parsed_fields', 'constants', 'fields', 'full_name', 'has_header', 'header_present', 'names', 'package', 'parsed_fields', 'short_name', 'text', 'types']
 
